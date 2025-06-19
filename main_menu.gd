518c12f9d44fa9e29bd0cc4f407a890d18e2d6f5
CaroleRofaeel <114744502+CaroleRofaeel@users.noreply.github.com>
2025-06-19 01:01:08 -0400

    Created Main Menu and Options Menu + Logic

---
+++ main_menu.gd
@@ -0,0 +1,36 @@
+extends Control
+
+@onready var start_button = $VBoxContainer/StartButton as Button
+@onready var options_button = $VBoxContainer/OptionsButton as Button
+@onready var quit_button = $VBoxContainer/QuitButton as Button
+@onready var vbox_container = $VBoxContainer as VBoxContainer
+@onready var options_menu = $options_menu as OptionsMenu
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	handle_connecting_signals()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_options_button_pressed() -> void:
+	vbox_container.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
+
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
+
+func on_exit_options_menu() -> void:
+	vbox_container.visible = true
+	options_menu.visible = false
+
+func handle_connecting_signals() -> void:
+	start_button.button_down.connect(_on_start_button_pressed)
+	options_button.button_down.connect(_on_options_button_pressed)
+	quit_button.button_down.connect(_on_quit_button_pressed)
+	options_menu.exit_options_menu.connect(on_exit_options_menu)
