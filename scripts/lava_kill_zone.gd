1f3bacb98ecb29ec7d513eb52c3e62f5e7957cda
AMALWorkPC <leasu.muhmeen@gmail.com>
2025-06-24 05:14:18 +0300

    feat: lava_kill_zone added and small edits

--- scripts/lava_kill_zone.gd
+++ scripts/lava_kill_zone.gd
@@ -1,16 +1,18 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var lava_sfx = $AudioStreamPlayer2D as AudioStreamPlayer2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("you died")
 		body.take_damage(200)
+		lava_sfx.play()
 		$CollisionShape2D.disabled = true
 		visible = false
 
 		# Set timer to sound duration, then start it
-		timer.wait_time = 0
+		timer.wait_time = lava_sfx.stream.get_length()
 		timer.start()
 
 func _on_WaitTimer_timeout() -> void:
