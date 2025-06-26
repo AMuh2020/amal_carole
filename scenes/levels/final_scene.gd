536abaf6263576ddaac8404e7c31b826bc3f2a02
CaroleRofaeel <114744502+CaroleRofaeel@users.noreply.github.com>
2025-06-26 12:52:03 -0400

    Quit Button

--- scenes/levels/final_scene.gd
+++ scenes/levels/final_scene.gd
@@ -1,7 +1,16 @@
 extends CanvasLayer
 @onready var label_2: Label = $ColorRect/Label2
+@onready var quit_button = $Button as Button
+
 
 func _ready() -> void:
 	label_2.visible = false
 	await get_tree().create_timer(3).timeout
 	label_2.visible = true
+	handle_connecting_signals()
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+	
+func handle_connecting_signals() -> void:
+	quit_button.button_down.connect(_on_quit_button_pressed)
